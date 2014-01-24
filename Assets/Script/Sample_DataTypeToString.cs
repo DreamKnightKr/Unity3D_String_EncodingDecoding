027a3213a91b0fab29a3e8048b89d4660a31a164
DreamKnightKr <dreamknightkr@gmail.com>
2014-01-24 19:01:46 +0900

    기본 데이터형 <-> String  기능 변환 기능 추가 * Int, Float(진행중)

--- Assets/Script/Sample_DataTypeToString.cs
+++ Assets/Script/Sample_DataTypeToString.cs
@@ -18,7 +18,7 @@ public class Sample_DataTypeToString : MonoBehaviour {
 	byte bByteVal = 0x41;	// ASCII = 'A'
 	char chCharVal = '핳'; 	//'A' : 2byte '핳' : 2byte
 	int nIntVal = 0xAC00;	// Unicode = '가'
-	float fFloatVal = 1.00101f;
+	float fFloatVal = 7.00101f;
 
 	// Use this for initialization
 	void Start () {
@@ -27,7 +27,7 @@ public class Sample_DataTypeToString : MonoBehaviour {
 	
 	void OnGUI()
 	{
-		float fPosX = 20, fPosY = 50, fYInterval = 40;
+		float fPosX = 300, fPosY = 50, fYInterval = 40;
 		int nYPosCount = 0;
 		string strOut = "";		
 		
@@ -69,6 +69,32 @@ public class Sample_DataTypeToString : MonoBehaviour {
 			Debug.Log(strOut);
 			txtOut.text += strOut;
 		}
+
+		nYPosCount++;
+		if (GUI.Button(new Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "Int Test"))
+		{
+			txtOut.text = "";
+			
+			strOut = ("Start Testing Int -> String(UTF-8) -> Int\n" +
+			          "Original Data : " + nIntVal.ToString());
+			strOut += "\n\n";
+			strOut += GetToStringDemos(nIntVal, DataType.Int);
+			Debug.Log(strOut);
+			txtOut.text += strOut;
+		}
+
+		nYPosCount++;
+		if (GUI.Button(new Rect(fPosX, fPosY + (fYInterval * nYPosCount), 200, 30), "Float Test"))
+		{
+			txtOut.text = "";
+			
+			strOut = ("Start Testing Float -> String(UTF-8) -> Float\n" +
+			          "Original Data : " + fFloatVal.ToString());
+			strOut += "\n\n";
+			strOut += GetToStringDemos(fFloatVal, DataType.Float);
+			Debug.Log(strOut);
+			txtOut.text += strOut;
+		}
 	}
 
 	string GetToStringDemos(object val, DataType type)
@@ -85,8 +111,100 @@ public class Sample_DataTypeToString : MonoBehaviour {
 		case DataType.CharUTF8:
 			strRet = GetToStringDemosCharUTF8((char)val);
 			break;
+		case DataType.Int:
+			strRet = GetToStringDemosInt((int)val);
+			break;
+		case DataType.Float:
+			strRet = GetToStringDemosFloat((float)val);
+			break;
 		}
 
+		return strRet;
+	}
+
+	string GetToStringDemosFloat(float val)
+	{
+		string strRet = "";
+		string strTemp = "";
+
+		// fot Normal Use
+		strRet += ("> String(digit)/revert Value : \n"
+		           + (strTemp = val.ToString())
+		           + "/"
+		           + float.Parse(strTemp));
+		strRet += "\n\n";
+
+		// for Check Hex Code
+		byte[] buff = System.BitConverter.GetBytes(val);
+		strTemp = System.BitConverter.ToString(buff).Replace("-", string.Empty);
+		double fTemp = System.BitConverter.ToDouble( GetHexStringToByte(strTemp, 8), 0);	//!! float를 double로 변화하는 상화이상 값이 이상해짐.
+		strRet += ("> Encoding ByteSteam string(Hex+IsLittleEndian)/revert Value(double) : \n" 
+		           + strTemp
+		           + "/"
+		           + (fTemp).ToString());
+
+		return strRet;
+	}
+
+	string GetToStringDemosInt(int val)
+	{
+		string strRet = "";
+		string strTemp = "";
+
+		// fot Normal Use
+		strRet += ("> String(digit)/revert Value : \n"
+		           + (strTemp = val.ToString())
+		           + "/"
+		           + int.Parse(strTemp));
+		strRet += "\n\n";
+
+		// fot Converting Char
+		strRet += ("> Char/revert Value : \n" 
+		           + ((char)val).ToString()
+		           + "/"
+		           + "(No Support)");
+		strRet += "\n\n";
+
+		// for Check Hex Code
+		byte[] buff = System.BitConverter.GetBytes(val);
+		strTemp = System.BitConverter.ToString(buff).Replace("-", string.Empty);
+		int nTemp = System.BitConverter.ToInt32( GetHexStringToByte(strTemp, 4), 0);
+		strRet += ("> Encoding ByteSteam string(Hex+IsLittleEndian)/revert Value : \n" 
+		           + strTemp
+		           + "/"
+		           + (nTemp).ToString());
+
+		return strRet;
+	}
+
+	string GetToStringDemosByte(byte val)
+	{
+		string strRet = "";
+		string strTemp = "";
+		int nTemp = 0;
+		
+		// fot Normal Use
+		strRet += ("> String(digit)/revert Value : \n"
+		           + (strTemp = val.ToString())
+		           + "/"
+		           + byte.Parse(strTemp));
+		strRet += "\n\n";
+		
+		// fot Converting Char
+		strRet += ("> Char/revert Value : \n" 
+		           + ((char)val).ToString()
+		           + "/"
+		           + "(No Support)");
+		strRet += "\n\n";
+		
+		// for Check Hex Code
+		byte[] buff = System.BitConverter.GetBytes(val);
+		strTemp = System.BitConverter.ToString(buff).Replace("-", string.Empty);
+		nTemp = System.BitConverter.ToInt16( GetHexStringToByte(strTemp), 0);
+		strRet += ("> Encoding ByteSteam string(Hex+IsLittleEndian)/revert Value : \n" 
+		           + strTemp
+		           + "/"
+		           + ((byte)nTemp).ToString());
 		return strRet;
 	}
 
@@ -101,7 +219,7 @@ public class Sample_DataTypeToString : MonoBehaviour {
 		strRet += ("> String(digit)/revert Value : \n"
 		           + nTemp.ToString()
 		           + "/"
-		           + GetStringFromUint(nTemp)
+		           + GetStringFromUTF8Uint(nTemp)
 		           );
 		strRet += "\n\n";
 
@@ -114,7 +232,7 @@ public class Sample_DataTypeToString : MonoBehaviour {
 		return strRet;
 	}
 
-	string GetStringFromUint(uint val)
+	string GetStringFromUTF8Uint(uint val)
 	{
 		// for protected 0 include case : ex) 0A 0B 0c 00
 
@@ -134,37 +252,6 @@ public class Sample_DataTypeToString : MonoBehaviour {
 		return System.Text.Encoding.UTF8.GetString(buffNew);
 	}
 
-	string GetToStringDemosByte(byte val)
-	{
-		string strRet = "";
-		string strTemp = "";
-		int nTemp = 0;
-
-		// fot Normal Use
-		strRet += ("> String(digit)/revert Value : \n"
-	           + (strTemp = val.ToString())
-				+ "/"
-				+ byte.Parse(strTemp));
-		strRet += "\n\n";
-
-		// fot Converting Char
-		strRet += ("> Char/revert Value : \n" 
-		           + ((char)val).ToString()
-					+ "/"
-					+ "(No Support)");
-		strRet += "\n\n";
-
-		// for Check Hex Code
-		byte[] buff = System.BitConverter.GetBytes(val);
-		strTemp = System.BitConverter.ToString(buff).Replace("-", string.Empty);
-		nTemp = System.BitConverter.ToInt16( GetHexStringToByte(strTemp), 0);
-		strRet += ("> Encoding ByteSteam string(Hex+IsLittleEndian)/revert Value : \n" 
-		           + (strTemp = System.BitConverter.ToString(buff).Replace("-", string.Empty))
-		           + "/"
-		           + ((byte)nTemp).ToString());
-		return strRet;
-	}
-
 	public static byte[] GetHexStringToByte(string hexString, int nPaddingSize = 0)
 	{
 		// Is LittleEndian type
@@ -172,7 +259,12 @@ public class Sample_DataTypeToString : MonoBehaviour {
 		int bytesCount = (hexString.Length) / 2;
 		int paddingCount = 0;
 		if(nPaddingSize > 0)
-			paddingCount += (hexString.Length) % nPaddingSize;
+		{
+			if(bytesCount > nPaddingSize)
+				paddingCount += bytesCount % nPaddingSize;
+			else
+				paddingCount = nPaddingSize - bytesCount;
+		}
 		byte[] bytes = new byte[bytesCount + paddingCount];
 
 		int x = 0;
